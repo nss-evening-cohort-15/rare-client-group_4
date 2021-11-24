486ec1bd3a3cc0288f62e46bcd553560df0dcc2c
Big-Red-Jon <jonathan.brooks21@yahoo.com>
2021-11-23 19:13:13 -0600

    Basic Post Setup

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,5 +1,7 @@
 import React from "react"
 import { Route } from "react-router-dom"
+import { PostProvider } from "./Post/PostProvider"
+import { PostList } from "./Post/PostList"
 
 export const ApplicationViews = () => {
     return <>
@@ -7,6 +9,11 @@ export const ApplicationViews = () => {
             margin: "5rem 2rem",
             lineHeight: "1.75rem"
         }}>
+            <PostProvider>
+                <Route path="/posts">
+                    <PostList />
+                </Route>
+            </PostProvider>
         </main>
     </>
 }
---
+++ src/components/Post/PostList.js
@@ -0,0 +1,46 @@
+import React, { useContext, useEffect } from "react"
+import { useHistory } from "react-router-dom"
+import { PostContext } from "./PostProvider"
+import "./Post.css"
+
+export const PostList = () => {
+
+    const { posts, getPosts } = useContext(PostContext)
+
+
+    useEffect(() => {
+        getPosts()
+    }, [])
+
+    const history = useHistory()
+
+    return (
+        <>
+            <h2>Posts</h2>
+            <button onClick={
+                () => history.push("/")
+            }>
+                Create New Post
+            </button>
+            <section className="posts">
+                {
+                    posts.map(post => {
+                        return (
+                            <div className="post">
+                                <div>
+                                    Title: {post.title}
+                                </div>
+                                <div>
+                                    Published: {post.publication_date}
+                                </div>
+                                <div>
+                                    {post.content}
+                                </div>
+                            </div>
+                        )
+                    })
+                }
+            </section>
+        </>
+    )
+}
