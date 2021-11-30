38375a5e09c0aecf7ed226bd5af99f092de14b42
Big-Red-Jon <jonathan.brooks21@yahoo.com>
2021-11-29 18:27:35 -0600

    All Posts Visable

---
+++ src/components/Post/PostForm.js
@@ -0,0 +1,106 @@
+import React, { useContext, useEffect, useState } from "react"
+import { PostContext } from "./PostProvider"
+import "./Post.css"
+import { useHistory, useParams } from "react-router-dom"
+
+
+export const PostForm = () => {
+    const { addPost, getPostById, updatePost } = useContext(PostContext)
+    // const { users, getUsers } = useContext(UserContext)
+
+    const [post, setPost] = useState({
+        id: 0,
+        user_id: 0,
+        category_id: 0,
+        title: "",
+        publication_date: "",
+        image_url: "",
+        content: "",
+        approved: false
+    })
+
+    const history = useHistory();
+    const { postId } = useParams();
+    const [isLoading, setIsLoading] = useState(true);
+    // const { userId } = useParams();
+    // const [value, setValue] = setState("")
+
+    const editCheckChange = (event) => {
+        const newPost = { ...post }
+        newPost[event.target.id] = event.target.value
+        setPost(newPost)
+    }
+
+    // useEffect(() => {
+    //     getUsers()
+    // }, [])
+
+    useEffect(() => {
+        if (postId) {
+            getPostById(postId)
+                .then(post => {
+                    setPost(post)
+                    setIsLoading(false)
+                })
+        } else {
+            setIsLoading(false)
+        }
+    }, [])
+
+    const currentUser = parseInt(sessionStorage.getItem("rare_user_id"))
+
+    const savePost = () => {
+        if (post.title === "" || post.content === "") {
+        } else {
+            setIsLoading(true);
+            if (postId) {
+                updatePost({
+                    id: parseInt(postId),
+                    user_id: post.user_id,
+                    category_id: post.category_id,
+                    title: post.title,
+                    publication_date: post.publication_date,
+                    image_url: post.image_url,
+                    content: post.content,
+                    approved: post.approved,
+                })
+                    .then(() => history.push(`/posts`))
+            } else {
+                addPost({
+                    user_id: post.user_id,
+                    category_id: post.category_id,
+                    title: post.title,
+                    publication_date: post.publication_date,
+                    image_url: post.image_url,
+                    content: post.content,
+                    approved: post.approved,
+                })
+                    .then(() => history.push(`/posts`))
+            }
+        }
+    }
+
+    return (
+        <>
+            <h2>{postId ? <>Edit Post</> : <> Add New Post</>}</h2>
+            <h3>TEST</h3>
+            <button
+                disabled={isLoading}
+                onClick={event => {
+                    event.preventDefault()
+                    savePost()
+                }}>
+                {postId ? <>Save Post</> : <>Add Post</>}
+            </button>{' '}
+        </>
+    )
+
+
+
+
+
+
+
+
+
+}
--- src/components/Post/PostProvider.js
+++ src/components/Post/PostProvider.js
@@ -7,8 +7,8 @@ export const PostProvider = (props) => {
 
     const getPosts = () => {
         return fetch("http://localhost:8088/posts")
-        .then(res => res.json())
-        .then(setPosts) 
+            .then(res => res.json())
+            .then(setPosts)
     }
 
     const addPost = postObj => {
@@ -19,11 +19,37 @@ export const PostProvider = (props) => {
             },
             body: JSON.stringify(postObj)
         })
-        .then(getPosts)
+            .then(getPosts)
     }
+
+    const updatePost = post => {
+        return fetch(`http://localhost:8088/posts/${post.id}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json"
+
+            },
+            body: JSON.stringify(post)
+        })
+            .then(getPosts)
+    }
+
+    const deletePost = postId => {
+        return fetch(`http://localhost:8088/posts/${postId}`, {
+            method: "DELETE"
+        })
+            .then(getPosts)
+    }
+
+    const getPostById = (postId) => {
+        return fetch(`http://localhost:8088/posts/${postId}
+        `)
+            .then(res => res.json())
+    }
+
     return (
         <PostContext.Provider value={{
-            posts, getPosts, addPost
+            posts, getPosts, addPost, updatePost, deletePost, getPostById
         }}>
             {props.children}
         </PostContext.Provider>
